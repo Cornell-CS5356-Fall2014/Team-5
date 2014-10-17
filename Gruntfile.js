c85d61ffd77085bfffcd2306a97cd7dc26e6f06f
Sean Herman <seanherman@gmail.com>
2014-10-17 16:24:48 -0400

    I hate you Gruntfile again

--- Gruntfile.js
+++ Gruntfile.js
@@ -125,6 +125,6 @@ module.exports = function(grunt) {
 
   // For Heroku users only.
   // Docs: https://github.com/linnovate/mean/wiki/Deploying-on-Heroku
-  grunt.registerTask('heroku:production', ['clean', 'mincss', 'uglify']); 
-  grunt.registerTask('heroku:development', ['clean', 'mincss']);
+  grunt.registerTask('heroku:production', ['clean', 'cssmin', 'uglify']); 
+  grunt.registerTask('heroku:development', ['clean', 'cssmin']);
 };
