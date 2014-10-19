d517b5986fe709520eeab0e84f81d821f8caa1e3
James <james.kizer@gmail.com>
2014-10-19 01:35:58 -0400

    Updated Facebook Callback URL

--- config/env/development.js
+++ config/env/development.js
@@ -20,7 +20,7 @@ module.exports = {
   facebook: {
     clientID: process.env['FB_CLIENT_ID'],
     clientSecret: process.env['FB_CLIENT_SECRET'],
-    callbackURL: 'http://localhost:3000/auth/facebook/callback'
+    callbackURL: 'http://dry-beach-2224-staging.herokuapp.com/auth/facebook/callback'
   },
   twitter: {
     clientID: 'DEFAULT_CONSUMER_KEY',
