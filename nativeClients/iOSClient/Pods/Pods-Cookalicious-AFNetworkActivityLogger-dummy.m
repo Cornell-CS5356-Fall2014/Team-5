f4b409efe52b355f7383bd738169f9e4eb90d850
James <james.kizer@gmail.com>
2014-10-22 18:43:31 -0400

    Initial version of the iOS client

---
+++ nativeClients/iOSClient/Pods/Pods-Cookalicious-AFNetworkActivityLogger-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Cookalicious_AFNetworkActivityLogger : NSObject
+@end
+@implementation PodsDummy_Pods_Cookalicious_AFNetworkActivityLogger
+@end
---
+++ nativeClients/iOSClient/Pods/Pods-Cookalicious-AFNetworking-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Cookalicious_AFNetworking : NSObject
+@end
+@implementation PodsDummy_Pods_Cookalicious_AFNetworking
+@end
