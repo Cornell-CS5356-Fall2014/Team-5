cfdbe22e6bd9bc2630bb3bed715ad9ca7c919d51
James <james.kizer@gmail.com>
2014-11-08 18:02:12 -0500

    Updated to transform returned journal entry object

--- packages/custom/journalEntries/server/controllers/journalEntries.js
+++ packages/custom/journalEntries/server/controllers/journalEntries.js
@@ -6,6 +6,7 @@
 var mongoose = require('mongoose'),
   JournalEntry = mongoose.model('JournalEntry'),
   Comment = mongoose.model('Comment'),
+  User = mongoose.model('User')
   util = require('util');
 
 // Find photo by id
@@ -44,8 +45,27 @@ function getEntries(req, res, userDicts) {
         error: 'Cannot list the journal entries'
       });
     }
-    console.log(util.inspect(journalEntries));
-    res.status(200).send(journalEntries);
+
+    //journalEntriesTransforms = []
+    promises = journalEntries.map(function(journalEntry) {
+      return new Promise(function(resolve, reject) {
+        journalEntryForPublicJSON(journalEntry, function(err, journalEntryJSON) {
+          if (err) reject(err);
+          //journalEntriesTransforms.push(journalEntryJSON);
+          console.log('Promise was successful!!');
+          console.log(util.inspect(journalEntries));
+          resolve(journalEntryJSON);
+        });
+      });
+    });
+
+    Promise.all(promises).then(function(journalEntriesTransforms) {
+      console.log('All Promise was successful!!');
+      console.log(util.inspect(journalEntries));
+      res.status(200).send(journalEntries);
+    });
+
+    
   });
 }
 
@@ -53,9 +73,17 @@ exports.create = function(req, res) {
  
   console.log('request body');
   console.log(util.inspect(arguments));
-  var journalEntry = new JournalEntry(req.body);
 
-  journalEntry.user = req.user;
+  var params = {
+                user : req.user,
+                photoList : req.body.photoList.parse(),
+                title : req.body.title,
+                detailText : req.body.detailText,
+                likerList : [],
+                commentList : []
+              }
+
+  var journalEntry = new JournalEntry(params);
 
   console.log('Creating journal entry');
   console.log(util.inspect(journalEntry));
@@ -67,8 +95,6 @@ exports.create = function(req, res) {
       });
     }
 
-
-
     console.log('Saved journal entry');
     console.log(util.inspect(journalEntry));
 
@@ -78,13 +104,97 @@ exports.create = function(req, res) {
       console.log('reloaded journal entry');
       console.log(util.inspect(j));
       res.json(journalEntry);
+
+      journalEntryForPublicJSON(journalEntry, function(err, journalEntryJSON) {
+        if (err) return next(err);
+        res.json(journalEntryJSON);
+      });
     });
-  
-
-    // res.json(journalEntry);
-
   });
 };
+
+
+function journalEntryForPublicJSON(journalEntry, cb) {
+
+  var commentQueryDictArray = journalEntry.comments.map(function(commentId) {
+    return {_id : commentId};
+  });
+
+  var photoQueryDictArray = journalEntry.photos.map(function(photoId) {
+    return {_id : photoId};
+  });
+
+  Comment
+    .find(commentQueryDictArray)
+    .exec(function(err, comments) {
+      if (err) {
+        cb(err, null);
+        return;
+      }
+
+      var commentUsers = comments.map(function(comment) {
+        return comment.user;
+      });
+
+      var users = new Set([journalEntry.user].concat(journalEntry.likerList).concat(commentUsers));
+      var userQueryDictArray = [];
+
+      users.forEach(function(userId) {
+        userQueryDictArray.push({_id : userId});
+      });
+
+      User
+        .find(userQueryDictArray)
+        .exec(function(err, users) {
+          if (err) {
+            cb(err, null);
+            return;
+          }
+
+          var userDictionary = {};
+          users.forEach(function(user) {
+            userDictionary = { user._id : {id: user._id, name: user.name, username: user.username}}
+          });
+
+          Photo
+            .find(photoQueryDictArray)
+            .exec(function(err, photos) {
+
+              if (err) {
+                cb(err, null);
+                return;
+              }
+
+              commentsArray = comments.map(function(comment) {
+                return {user: userDictionary[comment.user], createdDate: comment.createdDate, text: comment.text};
+              });
+
+              photosArray = photos.map(function(photo) {
+                return {user: userDictionary[photo.user], created: photo.created, fileName: photo.fileName, 
+                  caption: photo.caption, original: photo.original, thumbnail: photo.thumbnail};
+              });
+
+              likerArray = journalEntry.likerList.map(function(liker) {
+                return userDictionary[liker];
+              });
+
+              var journalEntryDictionary =  {
+                user: userDictionary[journalEntry.user],
+                photoList: photosArray,
+                title: journalEntry.title,
+                detailText: journalEntry.detailText,
+                likerList: likerArray,
+                commentList: commentsArray
+              }
+
+              cb(null, journalEntryDictionary);
+            });
+
+        });
+
+    });
+  
+}
 
 // exports.journalEntriesForUsers = function(req, res) {
 //   JournalEntry.find({'user' : req.user}).sort('-created').populate('user', 'name username').exec(function(err, photos) {
