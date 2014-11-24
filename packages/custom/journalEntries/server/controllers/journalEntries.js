49ffd3cbee95de7d477d789f2a32aff8e773d92a
James <james.kizer@gmail.com>
2014-11-23 19:39:44 -0500

    Added Photo model to journal entry controller

--- packages/custom/journalEntries/server/controllers/journalEntries.js
+++ packages/custom/journalEntries/server/controllers/journalEntries.js
@@ -50,40 +50,40 @@ function getEntries(req, res, userDicts) {
       });
     }
 
-    //res.status(200).send(journalEntries);
+    res.status(200).send(journalEntries);
 
     //journalEntriesTransforms = []
-    var promises = journalEntries.map(function(journalEntry) {
-      return new Promise(function(resolve, reject) {
+    // var promises = journalEntries.map(function(journalEntry) {
+    //   return new Promise(function(resolve, reject) {
 
-        // testFunction(journalEntry, function(err, journalEntryJSON) {
-        //   if (err) reject(err);
-        //   //journalEntriesTransforms.push(journalEntryJSON);
-        //   console.log('Promise was successful!!');
-        //   console.log(util.inspect(journalEntryJSON));
-        //   resolve(journalEntryJSON);
-        // });
+    //     // testFunction(journalEntry, function(err, journalEntryJSON) {
+    //     //   if (err) reject(err);
+    //     //   //journalEntriesTransforms.push(journalEntryJSON);
+    //     //   console.log('Promise was successful!!');
+    //     //   console.log(util.inspect(journalEntryJSON));
+    //     //   resolve(journalEntryJSON);
+    //     // });
 
-        journalEntryForPublicJSON(journalEntry, function(err, journalEntryJSON) {
-          if (err) reject(err);
-          //journalEntriesTransforms.push(journalEntryJSON);
-          console.log('Promise was successful!!');
-          console.log(util.inspect(journalEntryJSON));
-          resolve(journalEntryJSON);
-        });
+    //     journalEntryForPublicJSON(journalEntry, function(err, journalEntryJSON) {
+    //       if (err) reject(err);
+    //       //journalEntriesTransforms.push(journalEntryJSON);
+    //       console.log('Promise was successful!!');
+    //       console.log(util.inspect(journalEntryJSON));
+    //       resolve(journalEntryJSON);
+    //     });
 
 
-        // console.log('Promise was successful!!');
-        // console.log(util.inspect(journalEntry));
-        // resolve(journalEntry);
-      });
-    });
+    //     // console.log('Promise was successful!!');
+    //     // console.log(util.inspect(journalEntry));
+    //     // resolve(journalEntry);
+    //   });
+    // });
 
-    Promise.all(promises).then(function(journalEntriesTransforms) {
-      console.log('All Promise was successful!!');
-      console.log(util.inspect(journalEntriesTransforms));
-      res.status(200).send(journalEntriesTransforms);
-    });
+    // Promise.all(promises).then(function(journalEntriesTransforms) {
+    //   console.log('All Promise was successful!!');
+    //   console.log(util.inspect(journalEntriesTransforms));
+    //   res.status(200).send(journalEntriesTransforms);
+    // });
 
     
   });
