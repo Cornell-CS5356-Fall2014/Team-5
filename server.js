4a3d600e717dbe3714fd138cc36faa2661ae8676
James <james.kizer@gmail.com>
2014-12-06 17:13:10 -0500

    trying to get json working

--- server.js
+++ server.js
@@ -2,9 +2,16 @@
 
 // Requires meanio
 var mean = require('meanio');
+var bodyParser     = require('body-parser');
 
 // Creates and serves mean application
 mean.serve({ /*options placeholder*/ }, function(app, config) {
 	var port = config.https && config.https.port ? config.https.port : config.http.port;
 	console.log('Mean app started on port ' + port + ' (' + process.env.NODE_ENV + ')');
+
+	app.use(bodyParser.json());
+	app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
 });
