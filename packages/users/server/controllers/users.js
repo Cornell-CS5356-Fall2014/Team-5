a0feee1e665830cfa202a02c533583a38587d321
James <james.kizer@gmail.com>
2014-12-06 21:45:06 -0500

    trying to social

--- packages/users/server/controllers/users.js
+++ packages/users/server/controllers/users.js
@@ -198,7 +198,7 @@ exports.addUserToFollowing = function(req, res, next) {
 
 
 
-    console.log(util.inspect(req.user));
+    console.log(util.inspect(req.user.following));
     if (!req.user.following.prototype.contains(userToFollowId)) {
       req.user.following.push(userToFollow._id);
       userToFollow.followers.push(req.user._id);
