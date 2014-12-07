39789729b4ac30529fccbaae55e431449d5a45ac
Sean Herman <seanherman@gmail.com>
2014-12-06 19:05:52 -0500

    recipe search results

--- packages/custom/recipes/public/controllers/recipes.js
+++ packages/custom/recipes/public/controllers/recipes.js
@@ -1,10 +1,36 @@
 'use strict';
 
-angular.module('mean.recipes').controller('RecipesController', ['$scope', 'Global', 'Recipes',
-  function($scope, Global, Recipes) {
+angular.module('mean.recipes').controller('RecipesController', ['$scope', 'Global', 'Recipes', '$http',
+  function($scope, Global, Recipes, $http) {
     $scope.global = Global;
     $scope.package = {
       name: 'recipes'
+    };
+    $scope.recipes = null;
+
+    $scope.submit = function() {
+      console.log('Submitted');
+      if ($scope.query) {
+        console.log('Query is ' + $scope.query);
+        $http.post('/recipes/search', {query: $scope.query})
+            .success(function(data, status, headers, config) {
+              $scope.recipes = [];
+              //var result = angular.fromJson(data);
+              var result = data;
+              console.log(result);
+              if (result.matches) {
+                result.matches.forEach(function(one) {
+                  if (one.imageUrlsBySize && one.imageUrlsBySize[90]) {
+                    console.log('Image ' + one.imageUrlsBySize[90]);
+                  }
+                  $scope.recipes.push(one);
+                });
+              }
+            })
+            .error(function(data, status, headers, config) {
+              //
+            });
+      }
     };
   }
 ]);
